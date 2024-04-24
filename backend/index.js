d11599cacf1c63bdff46b3b9b71f05d8dae0c4ac
conf2rrum <conf2rrum@gmail.com>
2024-04-24 11:06:51 +0900

    Add a module that creates an assistant

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ const express = require('express');
 const cors = require('cors');
 // const serverless = require('serverless-http');
 const app = express()
-
+const fs = require('fs');
+const { toFile } = require('openai');
+const path = require('path');
 
 // CORS 이슈 해결
 // let corsOptions = {
-//     origin: 'https://chatdoge-173.pages.dev',
+//     origin: 'https://chatgenie.pages.dev',
 //     credentials: true
 // }
 
@@ -22,9 +24,45 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 const openai = new OpenAI({
     apiKey: API_KEY, // This is the default and can be omitted
 });
-  
+
+// Create assistant with file search
+async function createAssistant() {
+    // Create assistant
+    const myAssistant = await openai.beta.assistants.create({
+        instructions: "당신의 이름은 지니 입니다. 당신은 대한민국의 도로명교육 전문가 입니다. 당신은 초등학생들에게 대한민국의 도로명 체계에 대해 가르쳐야 합니다. 당신은 초등학생들이 이해하기 쉽게 알려줘야 합니다. 초등학생들에게 친근하게 알려주세요. 첨부된 파일에 있는 내용을 토대로 알려주세요. 학생들의 질문이 도로명에 대한 것이 아니라면, 도로명에 대해 질문하도록 말해주세요.",
+        name: "도로명교육 전문가",
+        tools: [{ type: "file_search" }],
+        model: "gpt-3.5-turbo-0125",
+    });
+
+    // Read files
+    const filesDir = './';
+    const fileNames = (await fs.promises.readdir(filesDir)).filter(name => name.endsWith('.pdf'));
+    console.log(fileNames);
+
+    const files = await Promise.all(fileNames.map(fileName => toFile(fs.createReadStream(path.join(filesDir, fileName)))));
+
+    // Create a vector store including our files
+    let vectorStore = await openai.beta.vectorStores.create({
+        name: "도로명교육 이론",
+    });
+
+    await openai.beta.vectorStores.fileBatches.uploadAndPoll(vectorStore.id, { files });
+
+    await openai.beta.assistants.update(myAssistant.id, {
+        tool_resources: { file_search: { vector_store_ids: [vectorStore.id] } },
+    });
+
+    console.log(myAssistant);
+}
+
+// Only excute to create assistant
+// Be sure not to forget to get assistant's ID
+// Assistant ID: asst_QSnIoI9IDSs9v2Zzv6zfTD1n
+// createAssistant();
+
 // POST method route
-app.post('/tellFortune', async (req, res) => {
+app.post('/ask', async (req, res) => {
     let { userMessage, threadId } = req.body
     const assistantId = "asst_QSnIoI9IDSs9v2Zzv6zfTD1n"
 
@@ -63,3 +101,5 @@ app.post('/tellFortune', async (req, res) => {
 
 // module.exports.handler = serverless(app);
 app.listen(3000)
+
+
